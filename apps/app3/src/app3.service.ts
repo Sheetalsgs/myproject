4263b2e6fea6fd24aa79409d2502c55859a31c22
sheetal-ayanworks <sheetal.shevalkar@ayanworks.com>
2023-06-09 16:37:34 +0530

    app3 chnages

--- apps/app3/src/app3.service.ts
+++ apps/app3/src/app3.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class App3Service {
   getHello(): string {
-    return 'Hello App3 Monoservice with new chnages 111111111 !!';
+    return 'Hello App3 Monoservice ...testing for chnages update';
   }
 }
